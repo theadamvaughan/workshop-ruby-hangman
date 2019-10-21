8972658d696b0de326b970c14d6623927896278b
theadamvaughan <adam_vaughan1982@hotmail.com>
2019-10-21 06:54:32 +0100

    added guess method. increments correct and incorrect guesses

--- hangman.rb
+++ hangman.rb
@@ -1,16 +1,35 @@
 class HangmanGame
-  def initialise(path:=.dictionary.txt, turns:=10, :word)
+  
+  def initialise(path:, turns: 10, word:)
     @path = path
-    @turns = turns
-    @word = word
+    @turns
+    @word = "hello"
   end
 end
-# # .............. End of Hangmnan Class ..................
 
-correct_guesses = 0
-incorrect_guess = 0
-guesses = []
+  @correct_guesses = 0
+  @incorrect_guess = 0
+  @guesses = []
 
+  
+  def guess(letter)
+    if @word.include?(letter) == true
+      puts "That's in the word and Adam needs to figure out what to do next"
+      @correct_guesses += 1
+    else
+      puts "That's not in the word you dumb fuck"
+      @incorrect_guess += 1
+    end
+    @guesses << letter
+    puts @correct_guesses
+    puts @incorrect_guess
+    puts @guesses
+  end
+
+  @word = "hello"
+  puts "please can I have your guess"
+  letter = gets.chomp
+  guess(letter)
 
 # def guesses(guesses)
 #   puts guesses
@@ -20,59 +39,43 @@ guesses = []
 # puts turns
 # end
 
-@word = "hello"
+  # # ............... need to edit this .............
+  def show
 
-
-def guess(letter)
-  if @words.contain?(letter) == true
-    puts "That's in the word and Adam needs to figure out what to do next"
-    correct_guesses += 1
-  else
-    puts "That's not in the word you dumb fuck"
-    incorrect_guess += 1
   end
-  # :turns -= 1
-end
+  # # ..............................................
 
-puts "please can I have your guess"
-letter = gets.chomp
+  def correct_guesses(correct_guesses)
+    puts "You have made #{correct_guesses} number of guesses"
+  end
 
-guess(letter)
+  def incorrect_guess(incorrect_guess)
+    puts "You have made #{incorrect_guess} number of incorrect guesses"
+  end
 
-# # ............... need to edit this .............
-def show
+  def guesses_left
 
-end
-# # ..............................................
+  end
 
-def correct_guesses(correct_guesses)
-  puts "You have made #{correct_guesses} number of guesses"
-end
+  def complete?
 
-def incorrect_guess(incorrect_guess)
-  puts "You have made #{incorrect_guess} number of incorrect guesses"
-end
+  end
 
-def guesses_left
+  def won?
 
-end
+  end
 
-def complete?
+  def over?
 
-end
+  end
 
-def won?
+  def lost?
 
-end
+  end
 
-def over?
+  def to_s
 
-end
+  end
 
-def lost?
 
-end
-
-def to_s
-
-end
+game = HangmanGame.new
