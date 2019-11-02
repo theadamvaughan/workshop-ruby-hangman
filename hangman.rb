163100155f71b5c3d1309d73ac9fb882076f733a
theadamvaughan <adam_vaughan1982@hotmail.com>
2019-11-02 15:39:41 +0000

    restarted the code, new methods added
    code can pick random word from text file
    asks for users input and confirms if it is in the word
    shows the user their previous guess
    some code not applicable

--- hangman.rb
+++ hangman.rb
@@ -1,81 +1,56 @@
-class HangmanGame
-  
-  def initialise(path:, turns: 10, word:)
-    @path = path
-    @turns
-    @word = "hello"
+def pick_random_line
+  chosen_line = nil
+  File.open './dictionary.txt' do |file|
+    file.each_with_index do |word, index|
+      chosen_line = word if rand < 1.0/(index+1)
+    end
+    return chosen_line
   end
 end
 
-  @correct_guesses = 0
-  @incorrect_guess = 0
-  @guesses = []
+class Game
+  @@turns = 10
+  @word = []
+  @@guesses = []
 
-  
-  def guess(letter)
-    if @word.include?(letter) == true
-      puts "That's in the word and Adam needs to figure out what to do next"
-      @correct_guesses += 1
-    else
-      puts "That's not in the word you dumb fuck"
-      @incorrect_guess += 1
-    end
-    @guesses << letter
-    puts @correct_guesses
-    puts @incorrect_guess
-    puts @guesses
+  attr_accessor :word, :guesses
+
+  def initialize(word)
+    @word = word
   end
 
-  @word = "hello"
-  puts "please can I have your guess"
-  letter = gets.chomp
-  guess(letter)
-
-# def guesses(guesses)
-#   puts guesses
-# end
-
-# def max_turns(turns)
-# puts turns
-# end
-
-  # # ............... need to edit this .............
-  def show
-
-  end
-  # # ..............................................
-
-  def correct_guesses(correct_guesses)
-    puts "You have made #{correct_guesses} number of guesses"
+  def print_word
+    puts "Your word is #{word}"
   end
 
-  def incorrect_guess(incorrect_guess)
-    puts "You have made #{incorrect_guess} number of incorrect guesses"
-  end
-
-  def guesses_left
-
-  end
-
-  def complete?
-
-  end
-
-  def won?
-
-  end
-
-  def over?
-
-  end
-
-  def lost?
-
+  def print_out
+    @word.each_char {|letter| print "-", " "}
+    puts
   end
 
   def to_s
-
+    return "(Thing.to_s):: The #{@name} is #{@description}"
   end
 
+  def guess(letter)
+    @@guesses << letter
+    puts "You have selected #{letter}"
+    if @word.include?(letter) == true
+      puts "That's in the word and Adam needs to figure out what to do next"
+    else
+      puts "That's not in the word you dumb fuck"
+    end
+  end
 
-game = HangmanGame.new
+  def start
+    puts "Can I have your guess"
+    user_guess = gets.chomp!
+    guess(user_guess)
+    puts "your guesses are #{@@guesses}"
+    puts @word
+  end
+end
+
+game = Game.new(pick_random_line)
+
+puts game.start
