f6df59c6a83f12ee799f8a76e6bf68231072a650
theadamvaughan <adam_vaughan1982@hotmail.com>
2019-11-05 16:31:28 +0000

    added methods to do with guesses. show method broken
    show method hasn't been working correctly. Method prints the chosen word after the "-"'s. Also contains 1 too many "-"'s

--- hangman.rb
+++ hangman.rb
@@ -10,8 +10,11 @@ end
 
 class Game
   @@turns = 10
+  @@incorrect_guesses = 0
+  @@correct_guesses = 0
   @word = []
   @@guesses = []
+  @@guesses_left = 10
 
   attr_accessor :word, :guesses
 
@@ -23,32 +26,50 @@ class Game
     puts "Your word is #{word}"
   end
 
-  def print_out
+  def show
     @word.each_char {|letter| print "-", " "}
-    puts
   end
 
   def to_s
-    return "(Thing.to_s):: The #{@name} is #{@description}"
+    puts "#{show}"
+    puts "#{guesses_left}#{incorrect_guesses}#{correct_guesses}"
   end
+
+  def incorrect_guesses
+    puts "Number of incorrect guesses: #{@@incorrect_guesses}"
+  end
+
+  def correct_guesses
+    puts "Number of correct guesses: #{@@correct_guesses}"
+  end
+
+  def guesses_left
+    puts "Number of guesses left: #{@@guesses_left}"
+  end
+
+    
 
   def guess(letter)
     @@guesses << letter
     puts "You have selected #{letter}"
     if @word.include?(letter) == true
       puts "That's in the word and Adam needs to figure out what to do next"
+      @@correct_guesses += 1
     else
       puts "That's not in the word you dumb fuck"
+      @@incorrect_guesses +=1
     end
   end
 
   def start
     puts "Can I have your guess"
     user_guess = gets.chomp!
+    @@guesses_left -= 1
     guess(user_guess)
     puts "your guesses are #{@@guesses}"
-    puts @word
+    puts to_s
   end
+
 end
 
 game = Game.new(pick_random_line)
